67c1d5216ae1bab22a8e62866d94a7a249115cbb
fedesuarez16 <fedesuarez16@gmail.com>
2024-02-01 15:51:23 -0300

    logos fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -58,18 +58,18 @@ desde la raiz
       </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
-        {socialMedia.map((social, index) => (
-          <img
-            key={social.id}
-            src={social.icon}
-            alt={social.id}
-            className={`w-[21px] h-[21px] object-contain cursor-pointer ${
-              index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
-            }`}
-            onClick={() => window.open(social.link)}
-          />
-        ))}
-      </div>
+  {socialMedia.map((social, index) => (
+    <img
+      key={social.id}
+      src={social.icon}
+      alt={social.id}
+      className={`w-[70px] h-[60px] object-contain cursor-pointer ${
+        index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+      }`}
+      onClick={() => window.open(social.link)}
+    />
+  ))}
+</div>
     </div>
     </div>
   </section>
